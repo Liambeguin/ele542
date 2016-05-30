2bffd1724d0ef25fb2ddb07054bf989a4c7b2a20
Liam BEGUIN <liambeguin@gmail.com>
2016-05-30 16:27:40 -0400

    main: timer1 channel compare test application

--- proto1/src/main.c
+++ proto1/src/main.c
@@ -4,6 +4,7 @@
 
 #include "inc/platform.h"
 #include "inc/uart.h"
+#include "inc/timer.h"
 
 int main(void) {
 
@@ -13,9 +14,10 @@ int main(void) {
 
 	while(1){
 
-
-		uart_send((uint8_t*)"a", 1);
 		leds_toggle(LED_0);
+		timer1_update_channel_compare(0, 0);
+		_delay_ms(1000);
+		timer1_update_channel_compare(1000, 0);
 		_delay_ms(1000);
 	}
 
