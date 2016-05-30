37d020b6009261b27e9ce354e7729ece78ead4a0
Liam BEGUIN <liambeguin@gmail.com>
2016-05-30 00:45:14 -0400

    main: Adding changes used for testing.

--- proto1/src/main.c
+++ proto1/src/main.c
@@ -7,18 +7,16 @@
 
 int main(void) {
 
-	uint8_t i=0;
-	// Use all pins of PORTB as outputs
-	DDRB = 0xff;
-
 
 	platform_init();
+	uart_send((uint8_t*)"\r\nInit Done!\r\n", 14);
 
-	PORTB = ~(0xa);
 	while(1){
-		uart_send('a');
-		_delay_ms(10);
-		i++;
+
+
+		uart_send((uint8_t*)"a", 1);
+		leds_toggle(LED_0);
+		_delay_ms(1000);
 	}
 
 	return EXIT_FAILURE;
